bb5ca960e57d28f89834a0ebc9e6cb3c5ff88878
SemiJunior-David-Bautista-Arroyo <davidbaar07@gmail.com>
2024-11-21 21:46:41 -0600

    Fix some mistakes

--- ventas/app/models/branchModel.py
+++ ventas/app/models/branchModel.py
@@ -4,6 +4,8 @@ from app.conection.connection import base
 
 class BranchModel(base):
 
+    __tablename__ = 'branch'
+
     id = Column(Integer, primary_key = True)
 
     branchname = Column(String)
